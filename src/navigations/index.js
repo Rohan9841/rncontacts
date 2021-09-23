416c0948b875e65513455f087668adc7fbca9b05
Rohan Maharjan <rma004@lsuhs.edu>
2021-09-23 15:04:14 -0500

    Added logged in user to the asyncStorage

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,17 +1,35 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./AuthNavigator";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import DrawerNavigator from "./DrawerNavigator";
 import { GlobalContext } from "../context/Provider";
+import { useEffect } from "react/cjs/react.development";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AppNavContainer = () => {
 
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
     const { authState: { isLoggedIn } } = useContext(GlobalContext);
-    console.log("isLoggedIn: ", isLoggedIn);
-    // const isLoggedIn = true;
+
+    const getUser = async () => {
+        try {
+            await AsyncStorage.getItem("user");
+            if (user) setIsAuthenticated(true);
+            else setIsAuthenticated(false);
+        } catch (error) { }
+    }
+
+    useEffect(() => {
+        getUser();
+        return () => {
+            console.log("cleanup in navigations/index.js after getting authenticated user.")
+        }
+    }, [])
+
     return (
         <NavigationContainer>
-            {isLoggedIn ? <DrawerNavigator /> : <AuthNavigator />}
+            {(isLoggedIn || isAuthenticated) ? <DrawerNavigator /> : <AuthNavigator />}
         </NavigationContainer>
     );
 }
